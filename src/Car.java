ddb491177203d2d64a928c8efae97d6d13055734
lebed <lebedamm@gmail.com>
2022-12-26 22:19:43 +0300

    JC 1

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    private String brand;
+    private String model;
+    private int engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+
+
+
+
+}
